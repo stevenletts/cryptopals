909696dcaa581e60188217091fdb8f1422fef877
stevenletts <s.spencerletts@gmail.com>
2025-01-18 07:52:32 +0000

    test: uncomment tests

--- packages/pkcs/pkcs_test.go
+++ packages/pkcs/pkcs_test.go
@@ -9,7 +9,7 @@ func TestPkcs7Pad(t *testing.T) {
 	var inp = "YELLOW SUBMARINE"
 	var expected = []byte("YELLOW SUBMARINE\x04\x04\x04\x04")
 
-	res := Pkcs7Pad([]byte(inp), 20)
+	res := Pad7([]byte(inp), 20)
 
 	if bytes.Compare(res, expected) != 0 {
 		t.Fatalf("failed to pad the input")
