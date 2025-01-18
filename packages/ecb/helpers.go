3842c176baacbc2d26a539195837d3269332d49a
stevenletts <s.spencerletts@gmail.com>
2025-01-18 08:20:16 +0000

    tidy

--- packages/ecb/helpers.go
+++ packages/ecb/helpers.go
@@ -24,71 +24,8 @@ func generateRandByteSlice(l int) []byte {
 	return b
 }
 
-// this is a wrapper for challenge 11
-func encryptionOracle(plaintext []byte) ([]byte, string) {
-	prependSlice := generateRandByteSlice(randmath.IntN(5) + 5)
-	appendSlice := generateRandByteSlice(randmath.IntN(5) + 5)
 
-	modifiedPlaintext := append(prependSlice, plaintext...)
-	modifiedPlaintext = append(modifiedPlaintext, appendSlice...)
-
-	key := generateRandByteSlice(16)
-
-	var ciphertext []byte
-	var err error
-	var encryptionModeRand int = randmath.IntN(2)
-	var mode string
-
-	switch encryptionModeRand {
-	case 0:
-		mode = "ECB"
-		ciphertext, err = EncryptAesEcb(modifiedPlaintext, key)
-
-		if err != nil {
-			panic(err)
-		}
-
-	case 1:
-		mode = "CBC"
-		iv := generateRandByteSlice(16)
-		ciphertext, err = EncryptAesCbc(modifiedPlaintext, iv, key)
-		if err != nil {
-			panic(err)
-		}
-	default:
-		panic(errors.New("this should not happen"))
-	}
-
-	return ciphertext, mode
-}
-
-type encryptionFunc func([]byte) []byte
-
-func makeSecretEncryptionFn() encryptionFunc {
-	data, err := os.ReadFile("./test_files/secret64.txt")
-
-	if err != nil {
-		panic(err)
-	}
-
-	secret, _ := base64.StdEncoding.DecodeString(string(data))
-	var key []byte = generateRandByteSlice(16)
-
-	encryptionFn := func(plaintext []byte) []byte {
-		var fulltext []byte = append(plaintext, secret...)
-
-		ciphertext, err := EncryptAesEcb(fulltext, key)
-
-		if err != nil {
-			panic(err)
-		}
-
-		return ciphertext
-	}
-
-	return encryptionFn
-}
-
+// fillByteSlice just fills a byte slice with a A byte to the length of the slice.
 func fillByteSlice(toFill []byte) []byte {
 	for p := 0; p < len(toFill); p++ {
 		toFill[p] = byte('A')
@@ -117,60 +54,47 @@ func discoverBlockSize(encryptor encryptionFunc) int {
 	}
 }
 
-func checkChunksForECB(chunks [][]byte) bool {
-	for i, c := range chunks {
-		for j, c2 := range chunks {
-			if i == j {
-				continue
-			}
 
-			if slices.Equal(c, c2) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-type StringKVParser map[string]string
-
-func (s StringKVParser) Get(key string) (string, bool) {
-	val, ok := s[key]
-	return val, ok
-}
-
-func (s StringKVParser) Set(key, value string) {
-	s[key] = value
-}
-
-func keyValueParser(str string) StringKVParser {
-	parser := StringKVParser{}
-
-	kvs := strings.Split(str, "&")
-
-	for _, kvPair := range kvs {
-		parts := strings.Split(kvPair, "=")
-		if len(parts) == 2 {
-			k, v := parts[0], parts[1]
-			parser.Set(k, v)
-		} else {
-			panic(errors.New("check why the kvPair is outputting multiple parts"))
-		}
-	}
-
-	return parser
-}
-
-func profileFor(email string) string {
-    // Sanitizing the email by removing '&' and '='
-    sanitizedEmail := strings.NewReplacer("&", "", "=", "").Replace(email)
-
-    // Build in the desired order explicitly:
-    kvPairs := []string{
-        fmt.Sprintf("email=%s", sanitizedEmail),
-        fmt.Sprintf("uid=%s", "10"),
-        fmt.Sprintf("role=%s", "user"),
+func checkChunksForECB[T comparable](chunks [][]T) bool {
+    for i, c := range chunks {
+        for j, c2 := range chunks {
+            if i == j {
+                continue
+            }
+            if slices.Equal(c, c2) {
+                return true
+            }
+        }
     }
-
-    return strings.Join(kvPairs, "&")
+    return false
 }
+
+
+
+type encryptionFunc func([]byte) []byte
+
+func makeSecretEncryptionFn() encryptionFunc {
+	data, err := os.ReadFile("./test_files/secret64.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	secret, _ := base64.StdEncoding.DecodeString(string(data))
+	var key []byte = generateRandByteSlice(16)
+
+	encryptionFn := func(plaintext []byte) []byte {
+		var fulltext []byte = append(plaintext, secret...)
+
+		ciphertext, err := EncryptAesEcb(fulltext, key)
+
+		if err != nil {
+			panic(err)
+		}
+
+		return ciphertext
+	}
+
+	return encryptionFn
+}
+
