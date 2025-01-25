eeba3720c20d58319cb0849a20af1e97ab17ec81
stevenletts <s.spencerletts@gmail.com>
2025-01-25 11:06:44 +0000

    challenge 15

--- packages/pkcs/pkcs.go
+++ packages/pkcs/pkcs.go
@@ -2,6 +2,7 @@ package pkcs
 
 import (
 	"bytes"
+	"errors"
 )
 
 // Pad7 takes a input text in byte form and a desired block size and pads the difference of the inp byte slice and the block size with
@@ -26,6 +27,26 @@ func Pad7Remove(data []byte, blockSize int) []byte {
 	return data[:len(data) - paddingLen]
 }
 
-//func HasPad7(data []byte) bool {
-	// get the last byte then check if that byte is repeated for the length of the byte value
-//}
+// HasPad7 will return if an input has padding (and it is valid) and if so the length
+func HasPad7(data []byte) (bool, int) {
+	last := data[len(data)-1]
+	length := int(last)
+
+	for i := len(data) -1; i >= len(data) -length; i-- {
+		b := data[i]
+		if b == last {
+			continue
+		}
+		return false, 0
+	}
+
+	return true, length
+}
+
+func StripPad7(data []byte, length int) ([]byte, error) {
+	if has, l := HasPad7(data); has {
+		return data[:len(data)-l], nil
+	}
+
+	return data, errors.New("is not pad7")
+}
--- packages/pkcs/pkcs_test.go
+++ packages/pkcs/pkcs_test.go
@@ -15,3 +15,37 @@ func TestPkcs7Pad(t *testing.T) {
 		t.Fatalf("failed to pad the input")
 	}
 }
+
+func TestHasPad7(t *testing.T) {
+    tests := []struct {
+        name       string
+        input      string
+        wantPadded bool
+        wantLen    int
+    }{
+        {
+            name:       "Valid padding",
+            input:      "ICE ICE BABY\x04\x04\x04\x04",
+            wantPadded: true,
+            wantLen:    4,
+        },
+        {
+            name:       "Invalid padding",
+            input:      "ICE ICE BABY\x05\x05\x05\x05",
+            wantPadded: false,
+            wantLen:    0,
+        },
+    }
+
+    for _, tc := range tests {
+        t.Run(tc.name, func(t *testing.T) {
+            gotPadded, gotLen := HasPad7([]byte(tc.input))
+            if gotPadded != tc.wantPadded {
+                t.Errorf("HasPad7(%q) = padded %v; want %v", tc.input, gotPadded, tc.wantPadded)
+            }
+            if gotLen != tc.wantLen {
+                t.Errorf("HasPad7(%q) = length %d; want %d", tc.input, gotLen, tc.wantLen)
+            }
+        })
+    }
+}
